2900747dca20813143596ab3174bef7d63e33581
juanRCoder <guillecarck@gmail.com>
2024-01-27 13:45:23 -0500

    fix: modify server and package

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { fileURLToPath } from "url";
 import connectDB from "../databases/db.js";
 import userRouter from "./routers/user.router.js";
-import cors from "cors";
+import path from "path";
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //aplicacion express y puerto de servidor node
 const app = express();
@@ -10,16 +16,15 @@ const PORT = process.env.PORT || 3006;
 //procesar las solicitudes JSON
 app.use(express.json());
 
-//interactuar con el puerto de React dev
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+// Servir archivos estaticos de la carpeta dist/
+app.use(express.static(path.resolve(__dirname, "../dist")));
+
 //rutas
 app.use(userRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Express server run: http://localhost:${PORT}`);
