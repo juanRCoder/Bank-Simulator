37a0e65f10a1963dd69540ebfed8c9fe4067bb3f
juanRCoder <guillecarck@gmail.com>
2024-01-29 16:05:47 -0500

    feat: bodyDashboard

--- src/Components/Body/BodyDashboard.jsx
+++ src/Components/Body/BodyDashboard.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import './BodyDashboard.css'
+import React from "react";
+import "./BodyDashboard.css";
 const BodyDashboard = () => {
   return (
     <>
-    <div className='dashboard'>
-        <div className='container'>
-            <div className='funcionalidades'>
-                <h1>¿Que Vamos Hacer Hoy?</h1>
-                <ul className='grupo-superior'>
-                  <li><button>Deposito</button></li>
-                  <li><button>Retiro</button></li>
-                  <li><button>Transacciones</button></li>
-                </ul>
-                <ul className='grupo-inferior'>
-                  <li><button>Mis Movimientos</button></li>
-                  <li><button>Estado de Cuenta</button></li>
-                </ul>
-            </div>
-            <div>
-                <p>mis productos  </p>
-            </div>
-            <div>cuentas de ahorro </div>
+      <div className="dashboard">
+        <div className="container">
+          <div className="funcionalidades">
+            <h1>¿Que Vamos Hacer Hoy?</h1>
+            <ul className="grupo-superior">
+              <li>
+                <button>Deposito</button>
+              </li>
+              <li>
+                <button>Retiro</button>
+              </li>
+              <li>
+                <button>Transacciones</button>
+              </li>
+            </ul>
+            <ul className="grupo-inferior">
+              <li>
+                <button>Mis Movimientos</button>
+              </li>
+              <li>
+                <button>Estado de Cuenta</button>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p>mis productos </p>
+          </div>
+          <div>cuentas de ahorro </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default BodyDashboard
+export default BodyDashboard;
