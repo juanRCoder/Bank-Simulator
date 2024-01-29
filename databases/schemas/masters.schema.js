b0b401c3ea3e072452338864d40a138a51293d1f
juanRCoder <guillecarck@gmail.com>
2024-01-29 12:52:04 -0500

    feat: new schema

--- databases/schemas/masters.schema.js
+++ databases/schemas/masters.schema.js
@@ -1,18 +1,23 @@
 import mongoose from "mongoose";
 
-const mastersSchema = new mongoose.Schema({
+const masterSchema = new mongoose.Schema({
   name: String,
   lastName: String,
   dni: {
     type: Number,
-    unique: true
+    required: true,
+    unique: true,
+  },
+  cardNumber: {
+    type: Number,
+    unique: true,
   },
   key: {
     type: Number,
     required: true,
-    unique: true
+    unique: true,
   },
 });
 
-const Masters = mongoose.model("Masters", mastersSchema, "masters");
+const Masters = mongoose.model("Masters", masterSchema, "masters");
 export default Masters;
