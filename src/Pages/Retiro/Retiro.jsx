26d232422819e503c2e153a18bea17dbe97ec9dc
oskar <omoralespj@gmail.com>
2024-02-10 16:12:20 -0500

    add styles

--- src/Pages/Retiro/Retiro.jsx
+++ src/Pages/Retiro/Retiro.jsx
@@ -107,7 +107,8 @@ const Retiro = () => {
               />
             </div>
           </div>
-          <div className=" mt-4 mb-14 text-xl  h-10 rounded-lg  ml-7  ">
+          <div className=" mb-14 text-xl  h-10 rounded-lg  ml-7  ">
+            <label htmlFor="withdrawal">Choose The Amount</label>
             <select
               className="text-base w-60 h-10 rounded-lg border border-slate-900"
               id="withdrawal"
@@ -116,7 +117,7 @@ const Retiro = () => {
               required
               onChange={handleChange}
             >
-              <option value="">Seleccione el monto</option>
+              <option value=""> Amount</option>
               <option value={20}>S/.20.00</option>
               <option value={50}>S/.50.00</option>
               <option value={100}>S/.100.00</option>
@@ -126,7 +127,7 @@ const Retiro = () => {
           <div className="relative h-11 w-80 ml-8  min-w-[200px]">
               <label
                 htmlFor="keyFour"
-                className="text-xl -mt-6 before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-cyan-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
+                className="text-xl -mt-3 before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-cyan-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
               >
                 Insert Your Passcode 4 digits
               </label>
@@ -136,11 +137,11 @@ const Retiro = () => {
                 value={keyFour}
                 onChange={handleChange}
                 placeholder="Insert your pasccode 4 digits"
-                className="bg-white peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-base font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                className=" mt-3 bg-white peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-base font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
               />
             </div>
 
-          <div className="p-6 pt-0">
+          <div className="p-6 pt-0 mt-8">
             <button
               data-ripple-light="true"
               type="submit"
