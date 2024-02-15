5417cdb2711c868ad77e266c40c24d72b1a20039
juanRCoder <guillecarck@gmail.com>
2024-02-15 11:56:54 -0500

    feat: deploy project

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Bank-Simulator/",
   css: {
     postcss: {
-      plugins: [tailwindcss]},
+      plugins: [tailwindcss],
+    },
   },
-})
+});
